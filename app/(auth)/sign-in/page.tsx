08042e6e23629836a6187f0f27b1a1531bbc1d3b
Thomas Testa <ttesta99@yahoo.com>
2025-05-20 15:27:29 -0400

    Auth Logic

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+import { authClient } from '@/lib/auth-client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
+  
+  const handleSignIn = async () => {
+    return await authClient.signIn.social({provider: 'google'});
+  }
+  
   return (
     <main className="sign-in">
       <aside className="testimonial">
@@ -48,7 +55,7 @@ const page = () => {
           <h1>SnapCast</h1>
         </Link>
         <p>Create and share your very first <span>SnapCast video</span> in no time!</p>
-        <button>
+        <button onClick={handleSignIn}>
           <Image 
             src="/assets/icons/google.svg"
             alt='google'
